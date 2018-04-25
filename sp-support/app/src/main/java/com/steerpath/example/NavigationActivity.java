d178aa88296dfce1c241fec6a82abd737cc282b9
Helena Meissner <helenameissner@yahoo.de>
2018-04-25 23:39:23 +0300

    round navi interface + navigation demo for DF

--- sp-support/app/src/main/java/com/steerpath/example/NavigationActivity.java
+++ sp-support/app/src/main/java/com/steerpath/example/NavigationActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,12 +21,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NavigationActivity extends AppCompatActivity {
-  
-    private String nextDestination = "";
-    private String landmark = "";
-    private String directionText = "";
-    private String directionImage = "";
-    private String arrivalTime = "";
 
     private static final int REQUEST_PERMISSIONS = 1;
     private static final int REQUEST_ENABLE_BT = 2;
@@ -40,7 +35,8 @@ public class NavigationActivity extends AppCompatActivity {
         setContentView(R.layout.navigation);
         switchToDefaultScreenMode();
         createDirectionToInterfaceMapping();
-        updateDirection("straight", "elevator");
+        //updateDirection("straight", "elevator");
+        demo();
     }
 
 
@@ -67,6 +63,12 @@ public class NavigationActivity extends AppCompatActivity {
         directionVisualOutputMap.put("left", R.mipmap.left_arrow);
         directionVisualOutputMap.put("straight", R.mipmap.straight_arrow);
         directionVisualOutputMap.put("back", R.mipmap.back_arrow);
+        directionVisualOutputMap.put("slight right", R.mipmap.slight_right);
+        directionVisualOutputMap.put("slight left", R.mipmap.slight_left);
+        directionVisualOutputMap.put("right u-turn", R.mipmap.right_u_turn);
+        directionVisualOutputMap.put("left u-turn", R.mipmap.left_u_turn);
+        directionVisualOutputMap.put("right s-turn", R.mipmap.right_s_turn);
+        directionVisualOutputMap.put("left s-turn", R.mipmap.left_s_turn);
 
         directionVisualOutputMap.put("elevator", R.mipmap.elevator);
         directionVisualOutputMap.put("escalator", R.mipmap.escalator);
@@ -79,6 +81,11 @@ public class NavigationActivity extends AppCompatActivity {
         directionTextOutputMap.put("left", "Turn left");
         directionTextOutputMap.put("straight", "Continue straight");
         directionTextOutputMap.put("back", "Turn backwards");
+        directionTextOutputMap.put("slight right", "Turn slight right");
+        directionTextOutputMap.put("right u-turn", "Make a right u-turn");
+        directionTextOutputMap.put("left u-turn", "Make a left u-turn");
+        directionTextOutputMap.put("right s-turn", "Make a right s-turn");
+        directionTextOutputMap.put("left s-turn", "Make a left s-turn");
 
         directionTextOutputMap.put("elevator", "towards the elevator");
         directionTextOutputMap.put("escalator", "towards the escalator");
@@ -90,16 +97,81 @@ public class NavigationActivity extends AppCompatActivity {
     /**
      * Update the navigation direction interface according to the given direction and landmark
      * @param direction direction to navigate to
-     * @param landmark landmark to navigate to
+     * @param landmark landmark to navigate to, if the is no landmark, pass landmark=""
      */
     public void updateDirection(String direction, String landmark) {
-        ImageView landm= (ImageView) findViewById(R.id.landmark);
+        ImageView landm = (ImageView) findViewById(R.id.landmark);
         ImageView directionImg = (ImageView) findViewById(R.id.direction_img);
         TextView directionText = (TextView) findViewById(R.id.direction_text);
-        
-        landm.setImageResource(directionVisualOutputMap.get(landmark));
+
+        if(landmark == "") {
+            landm.setImageResource(0);
+            directionText.setText(directionTextOutputMap.get(direction));
+        } else {
+            landm.setImageResource(directionVisualOutputMap.get(landmark));
+            directionText.setText(directionTextOutputMap.get(direction)+" "+
+                    directionTextOutputMap.get(landmark));
+        }
         directionImg.setImageResource(directionVisualOutputMap.get(direction));
-        directionText.setText(directionTextOutputMap.get(direction)+" "+directionTextOutputMap.get(landmark));
+    }
+
+    public void demo() {
+        updateDirection("right", "");
+        //try {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("straight", "");
+            }
+        }, 2000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("left", "");
+            }
+        }, 5000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("straight", "");
+            }
+        }, 7000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("left", "");
+            }
+        }, 12000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("right", "");
+            }
+        }, 14000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("straight", "");
+            }
+        }, 16000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("left u-turn", "escalator");
+            }
+        }, 27000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("right", "");
+            }
+        }, 34000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                updateDirection("straight", "");
+            }
+        }, 36000);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                ImageView directionImg = (ImageView) findViewById(R.id.direction_img);
+                TextView directionText = (TextView) findViewById(R.id.direction_text);
+                directionImg.setImageResource(directionVisualOutputMap.get("right"));
+                directionText.setText("Your gate is on your right");
+            }
+        }, 40000);
     }
     
 
