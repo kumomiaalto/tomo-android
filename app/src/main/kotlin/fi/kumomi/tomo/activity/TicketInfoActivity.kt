60cc9a49b3f83cea193e2f72d707cd4d05994a11
Jayanth Manklu <jayanthms7@gmail.com>
2018-05-29 09:28:44 +0530

    Ticket Activity - remove polling of ticket data

--- app/src/main/kotlin/fi/kumomi/tomo/activity/TicketInfoActivity.kt
+++ app/src/main/kotlin/fi/kumomi/tomo/activity/TicketInfoActivity.kt
@@ -9,55 +9,24 @@ import android.view.WindowManager
 import fi.kumomi.tomo.R
 import fi.kumomi.tomo.TomoApplication
 import fi.kumomi.tomo.model.AirlineTicket
-import fi.kumomi.tomo.observable.AirlineTicketObservable
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_ticket_info.*
 import org.joda.time.DateTimeZone
-import org.joda.time.LocalDateTime
 import org.joda.time.format.ISODateTimeFormat
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class TicketInfoActivity : AppCompatActivity() {
-    private var airlineTicketObservableSubject = PublishSubject.create<Boolean>()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_info)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
-        val app = applicationContext as TomoApplication
-
-        // Load ticket data from a global variable in app
-        if (app.ticket != null)
-            updateTicketData(app.ticket!!)
-
-        val airlineTicketObservable = AirlineTicketObservable.create()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .repeatWhen { it.delay(30, TimeUnit.SECONDS) }
-                .retryWhen { it.flatMap { Observable.timer(5, TimeUnit.SECONDS) } }
-
-        airlineTicketObservableSubject
-                .switchMap { if(it) airlineTicketObservable else Observable.never() }
-                .subscribe {
-                    app.ticket = it
-                    updateTicketData(it)
-                }
     }
 
     override fun onResume() {
         super.onResume()
-        airlineTicketObservableSubject.onNext(true)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        airlineTicketObservableSubject.onNext(false)
+        val app = applicationContext as TomoApplication
+        if (app.ticket != null)
+            updateTicketData(app.ticket!!)
     }
 
     fun launchDefaultActivity(view: View) {
